9a11aeccab9892f29a7070983a29894648414e84
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-01-25 09:00:42 -0800

    Add weather type calculation function and icon swapping

--- src/lib/sunlightCalc.test.js
+++ src/lib/sunlightCalc.test.js
@@ -3,11 +3,11 @@ import sunlightCalc from "./sunlightCalc.js";
 
 describe("my sunlightCalc function", () => {
 
-let sunriseJan24 = 1769269494; 
-let sunsetJan24 =  1769302652;
-//for the third optional variable to replace "Date.now()":
-let dateNowAfternoonJan24 =  1769292398558; //2pmish
-let earlyMorning = (sunriseJan24 * 1000) - 1;
+  let sunriseJan24 = 1769269494; 
+  let sunsetJan24 =  1769302652;
+  //for the third optional variable to replace "Date.now()":
+  let dateNowAfternoonJan24 =  1769292398558; //2pmish
+  let earlyMorning = (sunriseJan24 * 1000) - 1;
 //night (v1)
 //dawn
   it("returns time before sunrise", () => {
@@ -22,4 +22,33 @@ let earlyMorning = (sunriseJan24 * 1000) - 1;
 //dusk (v2)
 //night(v2)
 
+
+//sad paths?
 });
+
+
+// saving original function just in case
+// function sunlightCalc(sunrise, sunset){
+// 	//JavaScript uses ms, so adjust Unix timestamps:
+// 	const sunriseMs = sunrise * 1000;
+// 	const sunsetMs = sunset * 1000;
+// 	const dayLength = sunsetMs - sunriseMs;
+// 	const now = Date.now();
+// 	const MIN = 60 * 1000;
+// 	const DAWN_DURATION = 45 * MIN;
+// 	const MORNING_DURATION = 90 * MIN;
+// 	const DUSK_DURATION = 45 * MIN;
+// 	const PRE_SUNSET_GOLDEN = 90 * MIN;
+// 	const dawnStart = sunriseMs - DAWN_DURATION;
+// 	const morningEnd = sunriseMs + MORNING_DURATION;
+// 	const duskStart = sunsetMs - PRE_SUNSET_GOLDEN;
+// 	const duskEnd = sunsetMs + DUSK_DURATION;
+
+// 	if(now < dawnStart) return "night";
+// 	if(now < sunriseMs) return "dawn";
+// 	if(now < morningEnd) return "morning";
+// 	if(now < duskStart) return "afternoon";
+// 	if(now < sunsetMs) return "dusk";
+// 	if(now < duskEnd) return "dusk";
+// 	return "night";
+// };
